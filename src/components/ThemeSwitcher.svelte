ecb0ce81a1512087e78fac78e17e9a7a9158a269
ERJulian <julian.barrios@endrock.software>
2024-06-14 21:38:24 -0500

    Initial commit with some very basic stuff

---
+++ src/components/ThemeSwitcher.svelte
@@ -0,0 +1,27 @@
+<script>
+  let icons = {
+    light: '/moon-icon.png',
+    dark: '/sun-icon.png'
+  }
+
+  const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+  let currentIcon = darkMode ? icons.dark : icons.light
+  let currentTheme = darkMode ? 'dark' : 'light'
+
+  function changeTheme() {
+    if (currentIcon == icons.light) {
+      currentIcon = icons.dark
+      currentTheme = 'dark'
+    } else {
+      currentIcon = icons.light
+      currentTheme = 'light'
+    }
+    window.dispatchEvent(new CustomEvent('theme:change', {detail: currentTheme}))
+  }
+</script>
+
+
+<button class="header__theme-switcher" on:click={changeTheme}>
+  <img src={currentIcon} alt="theme icon">
+</button>
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Julián's blog";
+export const SITE_DESCRIPTION = "I rant about several topics";
+export const DARK_NAME = "Batman";
+export const LIGTH_NAME = "Julián";
