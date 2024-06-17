fca6354d374dbdf94a82b0bb9295f007887ba8e7
ERJulian <julian.barrios@endrock.software>
2024-06-16 21:04:21 -0500

    deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: "https://julianh99.github.io",
+  output: "static",
+  outDir: "docs",
   integrations: [mdx(), sitemap(), svelte()],
 });
